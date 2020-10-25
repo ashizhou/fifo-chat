f193ac6ec566c6add070999bf8ef0658ade5a238
ashizhou <70239764+ashizhou@users.noreply.github.com>
2020-10-24 21:43:49 -0500

    Update webchat.js

--- webchat.js
+++ webchat.js
@@ -68,7 +68,7 @@ function processResults(data) {
     // add to the bottom of the chat box
     console.log("got:" + data);
     $('#chatBox').append('<div class="media text-muted pt-3">');
-    $('#chatBox').append('<svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>')
+    $('#chatBox').append('<svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>');
     $('#chatBox').append('<p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">');
     $('#chatBox').append('<strong class="d-block text-gray-dark">@'+id+'</strong>');
     $('#chatBox').append(data);
